dd3dceb0d8fbc7d59b3996fc88d5f109496ca9fd
Chandan Rauniyar 💜 <143628422+chandan-rauniyar@users.noreply.github.com>
2026-08-07 11:38:45 +0530

    Add docker files

--- FRONTENDUI/src/api/auth.js
+++
@@ -1,4 +0,0 @@
-// api/auth.js
-import api from './axios'
-export const login = (email, password) =>
-  api.post('/auth/login', { email, password }).then(r => r.data.data)
--- FRONTENDUI/src/api/metrics.js
+++
@@ -1,3 +0,0 @@
-import api from './axios'
-
-export const getMetrics = () => api.get('/admin/metrics').then(r => r.data.data)
